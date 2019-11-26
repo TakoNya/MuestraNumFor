ff7b1c0819f4d469f6e2943ce4c169d8d4b70ef7
TakoNya <camilabelentaraborelli2@gmail.com>
2019-11-25 21:57:14 -0300

    MuestraNumFor

---
+++ src/muestranumforapp/MuestraNumForApp.java
@@ -0,0 +1,12 @@
+package muestranumforapp;
+
+public class MuestraNumForApp {
+
+        public static void main(String[] args) {
+        // Para cada num desde 1 hasta 100 incrementando 1 en cada vuelta.
+        for (int num = 1; num <= 100; num++){
+            System.out.println(num);
+        }
+    }
+    
+}
